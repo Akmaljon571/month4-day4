aaa4be607fa323cacf8aab95ec727778ad02b2ad
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-05-06 12:30:19 +0500

    day4

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Age-table
+let id = document.getElementById("age-table")
+console.log(id);
+
+
+// childNotes
+let jami = document.getElementById("id-list")
+console.log(jami.childNodes);
+
+// Age textli td
+let td = jami.previousSibling.previousSibling
+console.log(td);
+
+// name="search"
+let nam = document.getElementsByName('search')
+console.log(nam);
+
+// 1chi input
+let input = document.getElementById('label')
+console.log(input.childNodes);
+
+//2chi input
+let input2 = input.nextSibling.nextSibling
+console.log(input2);
